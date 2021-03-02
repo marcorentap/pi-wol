856e438670abb98477f0837970c446369d9faeaa
Marc Owen <marcowen20@gmail.com>
2021-03-02 20:23:31 +0800

    Finished core functionality (it now works in LAN)

--- remote-wol.py
+++ remote-wol.py
@@ -1,43 +1,78 @@
-from flask import Flask, session, redirect, url_for
+from flask import Flask, session, redirect, url_for, request
 from flask import render_template
+from wakeonlan import send_magic_packet
+from pythonping import ping
+from hashlib import sha256
+import sys
 
 app = Flask(__name__)
 app.secret_key = "13370x0539"
 
+TARGET_MAC_ADDRESS = "02:10:5C:6E:C6:70"
+TARGET_STATIC_IP  = "172.26.112.1"
+PASSWORD_FILE = "pass.txt"
+
 @app.route('/')
 def main_page():
+    wol_status = ""
     status = ""
-    ping_status = ""
 
-    if 'status' in session:
-        status = session['status']
+    if send_ping():
+        status = "ONLINE"
     else:
-        session['status'] = ""
-    if  'ping_status' in session:
-        ping_status = session['ping_status']
-    else:
-        session['ping_status'] = ""
+        status = "OFFLINE"
 
-    page = render_template('index.html', status=status, ping_status=ping_status)
+    if  'wol_status' in session:
+        wol_status = session['wol_status']
+    else:
+        session['wol_status'] = ""
+
+    page = render_template('index.html', status=status, wol_status=wol_status)
     session.pop('status')
-    session.pop('ping_status')
+    session.pop('wol_status')
+
     return page
 
-@app.route('/ping')
-def send_ping():
-    session['status'] = 'UNKNOWN'
-    session['ping_status'] = 'fail'
-    return redirect(url_for('main_page'))
-    # return "Ping sent!"
-
-@app.route('/wake')
+@app.route('/wake', methods=['GET', 'POST'])
 def wake_target():
-    session['status'] = 'OFFLINE'
-    session['ping_status'] = 'success'
-    return redirect(url_for('main_page'))
-    # return "Magic packet sent"
+    if request.method == 'POST':
+        if authenticate_user(request.form['password']):
+            send_magic_packet(TARGET_MAC_ADDRESS)
+            session['wol_status'] = 'success'
+        else:
+            session['wol_status'] = 'fail'
 
+        return redirect(url_for('main_page'))
+    else:
+        return redirect(url_for('auth_form'))
 
-# def send_ping():
-    
-# def send_magic_packet()
+@app.route('/auth')
+def auth_form():
+    return render_template('auth.html')
+
+def authenticate_user(inputPassword):
+    inputPassword = inputPassword.encode("utf-8")
+    hashedInput = sha256(inputPassword)
+    hashedDigest = hashedInput.hexdigest()
+
+    storedDigest = ""
+    with open(PASSWORD_FILE) as file:
+        storedDigest = file.read()
+
+    print("Input: {}\nStored: {}".format(hashedDigest, storedDigest), file=sys.stderr)
+    if storedDigest == hashedDigest:
+        return True
+    else:
+        return False
+
+def send_ping():
+    pingResponses = ping(TARGET_STATIC_IP, verbose=True, out=sys.stderr)
+    ping_status = pingResponses.success()
+    print("Ping Status: {}".format(ping_status), file=sys.stderr)
+    if ping_status:
+        session['status'] = 'ONLINE'
+        session['ping_status'] = 'success'
+    else:
+        session['status'] = 'OFFLINE'
+        session['ping_status'] = 'fail'
+    return ping_status 
