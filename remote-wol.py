6f424332e17ef9fbbabc91246c54cca9a1cff333
Marc Owen <marcowen20@gmail.com>
2021-03-02 15:24:30 +0800

    Made the front page dynamic

--- remote-wol.py
+++ remote-wol.py
@@ -1,22 +1,41 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for
 from flask import render_template
 
 app = Flask(__name__)
+app.secret_key = "13370x0539"
 
 @app.route('/')
-def show_main_page(inputStatus = None):
-    status = "UNKNOWN" 
-    if (inputStatus != None):
-        status = inputStatus
-    return render_template('index.html', status=status)
+def main_page():
+    status = ""
+    ping_status = ""
+
+    if 'status' in session:
+        status = session['status']
+    else:
+        session['status'] = ""
+    if  'ping_status' in session:
+        ping_status = session['ping_status']
+    else:
+        session['ping_status'] = ""
+
+    page = render_template('index.html', status=status, ping_status=ping_status)
+    session.pop('status')
+    session.pop('ping_status')
+    return page
 
 @app.route('/ping')
 def send_ping():
-    return "Ping sent!"
+    session['status'] = 'UNKNOWN'
+    session['ping_status'] = 'fail'
+    return redirect(url_for('main_page'))
+    # return "Ping sent!"
 
 @app.route('/wake')
 def wake_target():
-    return "Magic packet sent"
+    session['status'] = 'OFFLINE'
+    session['ping_status'] = 'success'
+    return redirect(url_for('main_page'))
+    # return "Magic packet sent"
 
 
 # def send_ping():
--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
+FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
 docker build -t remote-wol .
-docker run -p 80:5000 remote-wol
+docker run -p 80:5000 -v "D:\Projects\remote-wol:/app" remote-wol
